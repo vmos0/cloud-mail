9542ed1e6d69977d1bcd44e37e3dd80fe791b24f
duwid <142138034+vmos0@users.noreply.github.com>
2026-03-07 20:34:32 +0800

    Change GitHub OAuth redirect URL to login page

--- mail-worker/src/api/oauth-api.js
+++ mail-worker/src/api/oauth-api.js
@@ -19,7 +19,7 @@ app.get('/oauth/github/callback', async (c) => {
 	// 从URL参数中获取code
 	const code = c.req.query('code');
 	// 重定向到前端登录页面，带上code参数
-	const redirectUrl = `/github/callback?code=${code}`;
+	const redirectUrl = `/login?code=${code}&type=github`;
 	return c.redirect(redirectUrl, 302);
 });
 
