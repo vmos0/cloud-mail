d9eb7ebb164c2e36f2036d9757f4cda5b8eafa05
duwid <142138034+vmos0@users.noreply.github.com>
2026-03-07 21:30:07 +0800

    Update oauth-api.js

--- mail-worker/src/api/oauth-api.js
+++ mail-worker/src/api/oauth-api.js
@@ -19,7 +19,7 @@ app.get('/oauth/github/callback', async (c) => {
 	// 从URL参数中获取code
 	const code = c.req.query('code');
 	// 重定向到前端登录页面，带上code参数
-	const redirectUrl = `/github/callback?code=${code}`;
+	const redirectUrl = `${GH_CALLBACK_URL}?code=${code}`;
 	return c.redirect(redirectUrl, 302);
 });
 
